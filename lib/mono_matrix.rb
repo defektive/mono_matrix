ef2f5c23f2c16c27045e255596aefff7ac8c1782
defektive <sirbradleyd@gmail.com>
2014-12-19 16:03:18 -0700

    fies

--- lib/mono_matrix.rb
+++ lib/mono_matrix.rb
@@ -3,29 +3,29 @@ require 'serialport'
 
 class MonoMatrix
 
-  end_bytes = ["\xD5","\x7B"]
-  channels = {
-    "A": [
-      ["\x00", "\xFF"]
-      ["\x01", "\xFE"]
-      ["\x02", "\xFD"]
-      ["\x03", "\xFC"]
-    ],
+  # end_bytes = ["\xD5","\x7B"]
+  # channels = {
+  #   "A" => [
+  #     ["\x00", "\xFF"],
+  #     ["\x01", "\xFE"],
+  #     ["\x02", "\xFD"],
+  #     ["\x03", "\xFC"]
+  #   ],
 
-    "B": [
-      ["\x04", "\xFB"]
-      ["\x05", "\xFA"]
-      ["\x06", "\xF9"]
-      ["\x07", "\xF8"]
-    ]
-  }
+  #   "B" => [
+  #     ["\x04", "\xFB"],
+  #     ["\x05", "\xFA"],
+  #     ["\x06", "\xF9"],
+  #     ["\x07", "\xF8"]
+  #   ]
+  # }
 
   def initialize(tty)
     # Instance variables
     @tty = tty
   end
 
-  def switch(channel, input)
+  # def switch(channel, input)
 
     # A = 1 "\x00\xFF\xD5\x7B";
     # A = 2 "\x01\xFE\xD5\x7B";
@@ -37,14 +37,14 @@ class MonoMatrix
     # B = 4 "\x07\xF8\xD5\x7B";
 
 
-    bytes = channels[channel][input -1] + end_bytes if channels[channel] && channels[channel][input -1]
+    # bytes = channels[channel][input -1] + end_bytes if channels[channel] && channels[channel][input -1]
 
-    if bytes 
-      puts channel +" to "+ input.to_s
-      serialConnection = SerialPort.new @tty, 9600
-      bytes.each {|byte| serialConnection.write byte}
-      bytes.each {|byte| serialConnection.write byte}
-      serialConnection.close
-    end
-  end
+    # if bytes 
+    #   puts channel +" to "+ input.to_s
+    #   serialConnection = SerialPort.new @tty, 9600
+    #   bytes.each {|byte| serialConnection.write byte}
+    #   bytes.each {|byte| serialConnection.write byte}
+    #   serialConnection.close
+    # end
+  # end
 end
